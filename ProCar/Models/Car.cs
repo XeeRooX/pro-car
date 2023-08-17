68672c30d408184556e7867a758d9f57108956b3
CvCvCvv <decomen123@gmail.com>
2023-08-17 16:49:22 +0500

    add models car and cartype

---
+++ ProCar/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Principal;
+
+namespace ProCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; } = null!;
+        public double EngineСapacity { get; set; } // Объем двигатля
+        public int YearOfIssue { get; set; } //год выпуска
+        public int CostPerDay { get; set; } // стоимость за сутки
+        public int Deposit { get; set; } // залог
+        public int TimeDelayCost { get; set; } // стоимость задержки по времени
+
+        public CarType CarType { get; set; } = null!;
+
+    }
+}
