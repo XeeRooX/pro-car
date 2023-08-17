776a6578ff309615883360611582ac546a199940
XeeRooX <akkaunt7931@gmail.com>
2023-08-17 17:06:43 +0500

    Add DbContext class and add relationalsips

---
+++ ProCar/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProCar.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; } = null!;
+        public DbSet<CarType> CarTypes { get; set; } = null!;
+        public DbSet<DriveType> DriveTypes { get; set; } = null!;
+        public DbSet<GearboxType> GearboxTypes { get; set; } = null!;
+        public DbSet<FuelType> FuelTypes { get; set; } = null!;
+        public DbSet<Brand> Brands { get; set; } = null!;
+    }
+}
--- ProCar/Models/DriveType.cs
+++ ProCar/Models/DriveType.cs
@@ -5,5 +5,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
+
+        public List<Car> Cars { get; set; } = new();
+
     }
 }
