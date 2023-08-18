b02cf4dff2797fddde2fe3256a76cc9fcd8278e4
XeeRooX <akkaunt7931@gmail.com>
2023-08-19 01:02:41 +0500

    Add Delete and Edit pages

---
+++ ProCar/Pages/Admin/CarTypes/Delete.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProCar.Services;
+
+namespace ProCar.Pages.Admin.CarTypes
+{
+    public class DeleteModel : PageModel
+    {
+        private ICarTypeService _typeService;
+        public DeleteModel(ICarTypeService typeService)
+        {
+            _typeService = typeService;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost(int id) 
+        {
+            if(!_typeService.ElementExists(id))
+            {
+                return NotFound();
+            }
+
+            _typeService.DeleteType(id);
+
+            return RedirectToPage("/Admin/CarTypes/Index");
+        }    
+    }
+}
---
+++ ProCar/Pages/Admin/CarTypes/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProCar.Pages.Admin.CarTypes
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
