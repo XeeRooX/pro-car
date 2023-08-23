d1e5658f53f2be582bbe405977469e1acbdca5ca
CvCvCvv <decomen123@gmail.com>
2023-08-23 16:50:25 +0500

    add index and layout

---
+++ ProCar/Pages/Cars/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProCar.Pages.Cars
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
