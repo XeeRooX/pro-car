9e67d764a4796f70c22b9f464c53e4781888393f
CvCvCvv <decomen123@gmail.com>
2023-08-29 00:54:47 +0500

    fix brand view

--- ProCar/Pages/Cars/View.cshtml.cs
+++ ProCar/Pages/Cars/View.cshtml.cs
@@ -8,15 +8,19 @@ namespace ProCar.Pages.Cars
     public class ViewModel : PageModel
     {
         private readonly ICarsService _carsService;
+        private readonly IServerUploadService _uploadService;
 
         public Car Car { get; set; } = new();
-        public ViewModel(ICarsService carsService)
+        public int CountPhoto { get; set; }
+        public ViewModel(ICarsService carsService, IServerUploadService uploadService)
         {
             _carsService = carsService;
+            _uploadService = uploadService;
         }
         public void OnGet(int id)
         {
             Car = _carsService.GetById(id);
+            CountPhoto = _uploadService.CountCarPhotos(id);
         }
     }
 }
