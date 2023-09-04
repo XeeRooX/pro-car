ce2297f604acd02b797647248cd565d32a744f19
CvCvCvv <decomen123@gmail.com>
2023-09-04 20:03:18 +0500

    delete bot in index

--- ProCar/Pages/Index.cshtml.cs
+++ ProCar/Pages/Index.cshtml.cs
@@ -9,25 +9,22 @@ namespace ProCar.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ICarTypeService _carTypeService;
-        private readonly IBotMessageService _botService;
-
+     
         public List<CarType> CarTypes { get; set; }
         public int CountTypes { get; set; }
         public int CountCars { get; set; }
-        public IndexModel(ILogger<IndexModel> logger, ICarTypeService carTypeService, IBotMessageService botService)
+        public IndexModel(ILogger<IndexModel> logger, ICarTypeService carTypeService)
         {
             _carTypeService = carTypeService;
             _logger = logger;
-            _botService = botService;
         }
 
-        public async Task OnGet()
+        public void OnGet()
         {
             CarTypes = _carTypeService.GetTypesNotEmpty();
             CountTypes = 3;
             CountCars = 3;
 
-            await _botService.Send("Вася");
         }
 
     }
