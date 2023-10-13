01e28827a63473d02e865749d124743a94858207
XeeRooX <akkaunt7931@gmail.com>
2023-10-14 00:07:21 +0500

    last

--- ProCar/Dtos/CarAddDto.cs
+++ ProCar/Dtos/CarAddDto.cs
@@ -11,13 +11,13 @@ namespace ProCar.Dtos
         [Required]
         public string Model { get; set; } = null!;
         [Required]
-        [Range(0.0d,10.0d, ErrorMessage = "Неверно указана величина объема двигателя")]
+        [Range(0.0d, 10.0d, ErrorMessage = "Неверно указана величина объема двигателя")]
         public double EngineСapacity { get; set; }
         [Required]
-        [Range(1900, 9999, ErrorMessage ="Неверно указан год выпуска")]
+        [Range(1900, 9999, ErrorMessage = "Неверно указан год выпуска")]
         public int YearOfIssue { get; set; }
         [Required]
-        [Range(1, int.MaxValue, ErrorMessage ="Неверно указана велечина")]
+        [Range(1, int.MaxValue, ErrorMessage = "Неверно указана велечина")]
         public int CostPerDay { get; set; }
         [Required]
         [Range(0, int.MaxValue, ErrorMessage = "Неверно указана велечина")]
@@ -25,6 +25,11 @@ namespace ProCar.Dtos
         [Required]
         [Range(0, int.MaxValue, ErrorMessage = "Неверно указана велечина")]
         public int TimeDelayCost { get; set; }
+        [Required]
+        [Range(0.0d, 10000.0d, ErrorMessage = "Неверно указана велечина")]
+        public double? Horsepower { get; set; }
+        [Required]
+        public string? Equipment { get; set; }
 
         [Required]
         [Range(0, int.MaxValue)]
@@ -41,5 +46,8 @@ namespace ProCar.Dtos
         [Required]
         [Range(0, int.MaxValue)]
         public int BrandId { get; set; }
+
+        [Required]
+        public List<int> Colors { get; set; } = null!;
     }
 }
